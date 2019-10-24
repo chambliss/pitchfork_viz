8d526db4584d1ee52d9c7cc166203a0ff05d2dc1
Charlene Chambliss <chamblisscs@gmail.com>
2019-10-24 12:05:37 -0700

    Minor changes to preprocessing - thorough rewrite later

--- python/extract_features.py
+++ python/extract_features.py
@@ -1,30 +1,37 @@
-from utils.feat_ext import (get_table_contents, create_id_column, get_unique_artists, get_unique_genres, create_ents_df, create_pos_df, extract_features_from_reviews)
+from utils.feat_ext import (
+    get_table_contents,
+    create_id_column,
+    get_unique_artists,
+    get_unique_genres,
+    create_ents_df,
+    create_pos_df,
+    extract_text_features,
+)
 import os
 import pandas as pd
 import sqlite3
 import spacy
-from spacy.tokens.doc import Doc # mypy/typing import
+from spacy.tokens.doc import Doc  # mypy/typing import
 import time
 
 if __name__ == "__main__":
-    
+
     # Fetch contents of each table from the database
-    tables = ['artists', 'content', 'genres']
+    tables = ["artists", "content", "genres"]
     table_dfs = {table: get_table_contents(table) for table in tables}
-    
+
     # Write out DFs for unique artists and unique genres
-    get_unique_artists(table_dfs['artists']).to_csv(f"../data/artists.csv")
-    get_unique_genres(table_dfs['genres']).to_csv(f"../data/genres.csv")
-    
+    get_unique_artists(table_dfs["artists"]).to_csv(f"../data/artists.csv")
+    get_unique_genres(table_dfs["genres"]).to_csv(f"../data/genres.csv")
+
     # Extract text-based features for visualization later
-    combined_ents_df, combined_pos_df = extract_text_features(table_dfs['content'])
+    combined_ents_df, combined_pos_df = extract_text_features(table_dfs["content"])
 
     # Write out the final results
-    combined_ents_df.to_csv('../data/review_entities.csv')
-    combined_pos_df.to_csv('../data/review_poses.csv')
-    
+    combined_ents_df.reset_index(drop=True).to_csv("../data/review_entities.csv")
+    combined_pos_df.reset_index(drop=True).to_csv("../data/review_poses.csv")
+
     # Remove the last couple of tempfiles
-    filenames = [fn for fn in os.listdir("../data") if 'ents_' in fn or 'pos_' in fn]
+    filenames = [fn for fn in os.listdir("../data") if "ents_" in fn or "pos_" in fn]
     for tempfile in filenames:
-        os.remove(tempfile)
-    
+        os.remove(f"../data/{tempfile}")
