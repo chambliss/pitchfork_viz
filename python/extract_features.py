cb03b44302a32bb0eb20703524a40b1e1e61a30b
Charlene Chambliss <chamblisscs@gmail.com>
2019-10-24 18:00:44 -0700

    Rewrote text feature extraction -> 14.5x speedup

--- python/extract_features.py
+++ python/extract_features.py
@@ -3,8 +3,6 @@ from utils.feat_ext import (
     create_id_column,
     get_unique_artists,
     get_unique_genres,
-    create_ents_df,
-    create_pos_df,
     extract_text_features,
 )
 import os
@@ -25,13 +23,4 @@ if __name__ == "__main__":
     get_unique_genres(table_dfs["genres"]).to_csv(f"../data/genres.csv")
 
     # Extract text-based features for visualization later
-    combined_ents_df, combined_pos_df = extract_text_features(table_dfs["content"])
-
-    # Write out the final results
-    combined_ents_df.reset_index(drop=True).to_csv("../data/review_entities.csv")
-    combined_pos_df.reset_index(drop=True).to_csv("../data/review_poses.csv")
-
-    # Remove the last couple of tempfiles
-    filenames = [fn for fn in os.listdir("../data") if "ents_" in fn or "pos_" in fn]
-    for tempfile in filenames:
-        os.remove(f"../data/{tempfile}")
+    extract_text_features(table_dfs["content"], "review_pos.txt", "review_ents.txt")
--- python/utils/feat_ext.py
+++ python/utils/feat_ext.py
@@ -4,7 +4,6 @@ import sqlite3
 import spacy
 from spacy.tokens.doc import Doc
 import time
-from typing import Tuple
 
 
 create_id_column = lambda df: pd.Index(range(df.shape[0]), name="id")
@@ -41,98 +40,53 @@ def get_unique_artists(artists_df: pd.DataFrame) -> pd.DataFrame:
 def get_unique_genres(genres_df: pd.DataFrame) -> pd.DataFrame:
 
     unique_genres = genres_df["genre"].drop_duplicates().to_frame("genre")
-
     unique_genres.index = create_id_column(unique_genres)
 
     return unique_genres
 
 
-# I don't think I actually need this function - we can leave this info in the reviews df
-def get_unique_authors(reviews_df: pd.DataFrame) -> pd.DataFrame:
-
-    unique_authors = (
-        reviews_df[["author", "author_type"]].drop_duplicates().sort_values(by="author")
-    )
-
-    unique_authors.index = create_id_column(unique_authors)
-
-    return unique_authors
-
-
-def create_ents_df(spacy_doc: Doc, review_id: int) -> pd.DataFrame:
-
-    desired_labels = ["ORG", "PERSON", "GPE", "NORP", "EVENT"]
-    ents_in_text = [
-        (review_id, ent.text, ent.label_)
-        for ent in spacy_doc.ents
-        if ent.label_ in desired_labels
-    ]
-
-    ents_df = pd.DataFrame(ents_in_text)
-    cols = ["review_id", "entity", "spacy_pred"]
-
-    if not ents_df.empty:
-        ents_df.columns = cols
-        ents_df = ents_df.drop_duplicates(subset="entity")
-    else:
-        ents_df = pd.DataFrame(columns=cols)
-
-    return ents_df
-
-
-def create_pos_df(spacy_doc: Doc, review_id: int) -> pd.DataFrame:
+def extract_text_features(content_df: pd.DataFrame, pos_file: str, ents_file: str):
 
     """
-    Creates a DF of every adjective, noun, and verb occurring in a given
-    review. Takes a spacy Doc object and returns a DataFrame.
+    Extracts the parts-of-speech and entity features using spaCy. This function
+    does not return anything, it just writes the files out to the specified paths
+    in the /data/ directory.
     """
-
-    desired_pos = ["ADJ", "NOUN", "VERB"]
-    pos_tags_in_text = [
-        (review_id, tok.text, tok.pos_) for tok in spacy_doc if tok.pos_ in desired_pos
-    ]
-
-    pos_df = pd.DataFrame(pos_tags_in_text).drop_duplicates()
-    cols = ["review_id", "word", "pos"]
-
-    if not pos_df.empty:
-        pos_df.columns = cols
-    else:
-        pos_df = pd.DataFrame(columns=cols)
-
-    return pos_df
-
-
-def extract_text_features(
-    content_df: pd.DataFrame
-) -> Tuple[pd.DataFrame, pd.DataFrame]:
 
     start_time = time.time()
 
-    # Prepare spacy model and document pipeline
+    # Prepare spaCy model and document pipeline
     nlp = spacy.load("en_core_web_lg")
     review_ids, texts = content_df["reviewid"].values, content_df["content"].values
-    doc_generator = nlp.pipe(texts, disable=["parser"], n_threads=16, batch_size=50)
+    doc_generator = nlp.pipe(texts, disable=["parser"], batch_size=32)
     doc_pipeline = zip(review_ids, doc_generator)
+    pos_path, ents_path = f"../data/{pos_file}", f"../data/{ents_file}"
 
-    # Create empty dfs to append to
-    combined_ents_df, combined_pos_df = pd.DataFrame(), pd.DataFrame()
+    with open(ents_path, "w") as pos_file, open(pos_path, "w") as ents_file:
 
-    # Main loop: Extract entities and interesting words (adjs, verbs, nouns)
-    # from each review. "pos": "part of speech"
-    for i, (review_id, doc) in enumerate(doc_pipeline, start=1):
-        combined_ents_df = combined_ents_df.append(create_ents_df(doc, review_id))
-        combined_pos_df = combined_pos_df.append(create_pos_df(doc, review_id))
+        for i, (review_id, doc) in enumerate(doc_pipeline, start=1):
 
-        # Every 1000 docs, save intermediate CSVs and report time elapsed
-        if i % 1000 == 0:
-            combined_ents_df.to_csv(f"../data/ents_{i}.csv")
-            combined_pos_df.to_csv(f"../data/pos_{i}.csv")
-            print(f"Finished {i} reviews. Time elapsed: {time.time() - start_time}s")
+            desired_pos = ["ADJ", "NOUN", "VERB"]
+            pos_tags_in_text = [
+                ",".join([str(review_id), tok.text, tok.pos_, "\n"])
+                for tok in doc
+                if tok.pos_ in desired_pos
+            ]
 
-            # Remove the last round's temp csv files
-            if os.path.exists(f"../data/ents_{i - 1000}.csv"):
-                os.remove(f"../data/ents_{i - 1000}.csv")
-                os.remove(f"../data/pos_{i - 1000}.csv")
+            desired_labels = ["ORG", "PERSON", "GPE", "NORP", "EVENT"]
+            ents_in_text = [
+                ",".join([str(review_id), ent.text, ent.label_, "\n"])
+                for ent in doc.ents
+                if ent.label_ in desired_labels
+            ]
 
-    return combined_ents_df, combined_pos_df
+            pos_file.writelines(pos_tags_in_text)
+            ents_file.writelines(ents_in_text)
+
+            # Every 1000 docs, report time elapsed
+            if i % 1000 == 0:
+                print(
+                    f"Finished {i} reviews. Time elapsed: {time.time() - start_time}s"
+                )
+
+    return None
